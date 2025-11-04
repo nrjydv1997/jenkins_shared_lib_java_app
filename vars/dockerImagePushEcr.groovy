d21eaf3d7920fde5609a0c1e236dc37cb85f3a2b
nrjydv1997 <nrjydv1997@gmail.com>
2025-11-04 14:16:46 +0530

    docker image scanand push function added

--- vars/dockerImagePushEcr.groovy
+++ vars/dockerImagePushEcr.groovy
@@ -1,6 +1,11 @@
 def call(String aws_account_id, String region, String ecrRepoName){
-    sh """
-    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-    docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest
-    """
+    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+           sh"""
+            aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+            aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+            aws configure set default.region ${params.region}
+            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+            docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest
+           """
+    }          
 }
